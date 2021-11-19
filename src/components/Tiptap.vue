2e3717b2eb12274664fd8fb627c408939ca2f570
Hans Pagel <hans.pagel@ueber.io>
2021-11-19 12:28:22 +0100

    add collab cursors

--- src/components/Tiptap.vue
+++ src/components/Tiptap.vue
@@ -1,13 +1,14 @@
 <template>
   <h1>Editor</h1>
-  <div class="editor">
+  <div class="editor" v-if="editor">
     <editor-toolbar :editor="editor" />
     <editor-content :editor="editor" />
-  </div>
-  <h1>Another Editor</h1>
-  <div class="editor">
-    <editor-toolbar :editor="anotherEditor" />
-    <editor-content :editor="anotherEditor" />
+    <ul v-for="user in editor.storage.collaborationCursor.users">
+      <li>
+        <img :src="user.avatar" width="30" alt="">
+        {{ user.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -16,6 +17,7 @@ import { Editor, EditorContent } from '@tiptap/vue-3'
 import StarterKit from '@tiptap/starter-kit'
 import EditorToolbar from './EditorToolbar.vue'
 import Collaboration from '@tiptap/extension-collaboration'
+import CollaborationCursor from '@tiptap/extension-collaboration-cursor'
 import * as Y from 'yjs'
 import { HocuspocusProvider } from '@hocuspocus/provider'
 
@@ -36,22 +38,10 @@ export default {
   data() {
     return {
       editor: null,
-      anotherEditor: null,
     }
   },
 
   mounted() {
-    this.anotherEditor = new Editor({
-      extensions: [
-        StarterKit.configure({
-          history: false,
-        }),
-        Collaboration.configure({
-          document: ydoc,
-        }),
-      ],
-    })
-
     this.editor = new Editor({
       extensions: [
         StarterKit.configure({
@@ -60,13 +50,25 @@ export default {
         Collaboration.configure({
           document: ydoc,
         }),
+        CollaborationCursor.configure({
+          provider: provider,
+          user: {
+            name: 'Kevin Jahns',
+            color: '#ffcc00',
+          }
+        })
       ],
+    })
+
+    this.editor.commands.updateUser({
+      name: prompt('What’s your name?'),
+      color: '#ffcc00',
+      avatar: 'https://unavatar.io/github/hanspagel',
     })
   },
 
   beforeUnmount() {
     this.editor.destroy()
-    this.anotherEditor.destroy()
     provider.destroy()
   },
 }
@@ -155,5 +157,32 @@ button {
     border-top: 2px solid rgba(#0D0D0D, 0.1);
     margin: 2rem 0;
   }
+}
+
+/* Give a remote user a caret */
+.collaboration-cursor__caret {
+  position: relative;
+  margin-left: -1px;
+  margin-right: -1px;
+  border-left: 1px solid #0D0D0D;
+  border-right: 1px solid #0D0D0D;
+  word-break: normal;
+  pointer-events: none;
+}
+
+/* Render the username above the caret */
+.collaboration-cursor__label {
+  position: absolute;
+  top: -1.4em;
+  left: -1px;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  user-select: none;
+  color: #0D0D0D;
+  padding: 0.1rem 0.3rem;
+  border-radius: 3px 3px 3px 0;
+  white-space: nowrap;
 }
 </style>
